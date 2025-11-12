f95d3bbaa80b5f7f638f4b1641f0e0cfc4d89959
haggqvist459 <48819082+haggqvist459@users.noreply.github.com>
2025-11-12 21:46:49 +0100

    minor error page stylefixes

--- src/components/ui/ErrorComponent.tsx
+++ src/components/ui/ErrorComponent.tsx
@@ -8,8 +8,14 @@ const ErrorComponent = ({ errorMessage }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[300px]">
       <ErrorIcon />
-      <span className="text-primary-text mt-2">Something went wrong. <br /> Try reloading the page.</span>
-      <span className="text-primary-text mt-2">{errorMessage}</span>
+      <span className="text-primary-text mt-2">Something went wrong. </span>
+      {errorMessage &&
+        <>
+          <span className="text-primary-text"><br /> Error message: <br/></span>
+          <span className="text-primary-text mt-2">{errorMessage}</span>
+        </>
+      }
+
     </div>
   );
 }
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
+import { ROUTES } from "@/utils";
+import { PageContainer, Heading, ErrorComponent } from "@/components";
+
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <PageContainer>
+      <div className="mt-5 px-5">
+        <Heading title="Error" headingType="section-heading" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <ErrorComponent />
+        <span>Return to <Link to={ROUTES.HOME} className="underline decoration">home page</Link> </span>
+      </div>
+    </PageContainer>
   );
 }
 
